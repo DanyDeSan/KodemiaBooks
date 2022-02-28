c8d7f0c50691f168b7f400dbde5c9eb11d6c4fb9
Dany De San <ldanieldesanpedro@gmail.com>
2022-02-27 23:34:09 -0600

    Refactored Log In Screen with new style

--- BooksKodemia/Controller/LoginViewController.swift
+++ BooksKodemia/Controller/LoginViewController.swift
@@ -11,13 +11,17 @@ class LoginViewController: UIViewController {
     
     // Una variable lazy, se crea en el momento que
     // la invocamo por primera vez.
-    private lazy var userInputTextField: UITextField = UITextField()
+    private lazy var userInputTextField: InputTextField = InputTextField(frame: CGRect(), placeHolder: Constants.userString)
     
-    private lazy var passwordInputTextField: UITextField = UITextField()
+    private lazy var passwordInputTextField: InputTextField = InputTextField(frame: CGRect(), placeHolder: Constants.passwordString)
     
-    private lazy var loginButton: UIButton = UIButton()
+    private lazy var loginButton: FilledButton = FilledButton(title: Constants.logIn, frame: CGRect())
     
-    private lazy var signInButton: UIButton = UIButton()
+    private lazy var signInButton: UnfilledButton = UnfilledButton(title: Constants.signIn, frame: CGRect())
+    
+    private lazy var textFieldsStackView: UIStackView = UIStackView()
+    
+    private lazy var buttonsStackView: UIStackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,54 +32,52 @@ class LoginViewController: UIViewController {
     private func initUI() {
         self.view.backgroundColor = .systemBackground
         
-        self.view.addSubview(userInputTextField)
-        // Para que los constraints que definamos en código se tomen en cuenta.
-        userInputTextField.translatesAutoresizingMaskIntoConstraints = false
-        userInputTextField.placeholder = Constants.userString
-        userInputTextField.layer.borderWidth = 1
-        // Agregar constraints del User Input
-        NSLayoutConstraint.activate([
-            userInputTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            userInputTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userInputTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.widthProportion)
-        ])
-        // Configurando password textfield
-        self.view.addSubview(passwordInputTextField)
-        passwordInputTextField.translatesAutoresizingMaskIntoConstraints = false
-        passwordInputTextField.placeholder = Constants.passwordString
-        passwordInputTextField.layer.borderWidth = 1
+        // Textfields array
+        let textFields: [InputTextField] = [userInputTextField,passwordInputTextField]
+        // TextFields Stack view
+        view.addSubview(textFieldsStackView)
+        textFieldsStackView.axis = .vertical
+        textFieldsStackView.spacing = Constants.padding
+        textFieldsStackView.alignment = .fill
+        textFieldsStackView.distribution = .fillEqually
+        
+        textFields.forEach { inputTextField in
+            textFieldsStackView.addArrangedSubview(inputTextField)
+        }
+        
+        textFieldsStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            passwordInputTextField.topAnchor.constraint(equalTo: userInputTextField.bottomAnchor, constant: Constants.padding),
-            passwordInputTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            passwordInputTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.widthProportion)
+            textFieldsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textFieldsStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textFieldsStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.widthProportion)
         ])
         
-        //Configurando boton de sign in
-        self.view.addSubview(signInButton)
-        self.signInButton.translatesAutoresizingMaskIntoConstraints = false
-        signInButton.setTitle(Constants.signIn, for: .normal)
-        signInButton.backgroundColor = Constants.buttonBackgroundColor
-        NSLayoutConstraint.activate([
-            signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.padding),
-            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.widthProportion),
-            signInButton.heightAnchor.constraint(equalToConstant: Constants.buttonSize)
-        ])
+        textFields.forEach { inputTextfield in
+            inputTextfield.translatesAutoresizingMaskIntoConstraints = false
+            inputTextfield.heightAnchor.constraint(equalToConstant: Constants.textFieldHeight).isActive = true
+        }
         
-        //Configurando boton de login
-        self.view.addSubview(loginButton)
-        self.loginButton.translatesAutoresizingMaskIntoConstraints = false
+        let buttons: [UIButton] = [loginButton,signInButton]
         
-        self.loginButton.setTitle(Constants.logIn, for: .normal)
-        self.loginButton.backgroundColor = Constants.buttonBackgroundColor
+        view.addSubview(buttonsStackView)
+        buttonsStackView.spacing = Constants.padding
+        buttonsStackView.axis = .vertical
+        buttonsStackView.alignment = .fill
+        buttonsStackView.distribution = .fillEqually
+        buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            loginButton.bottomAnchor.constraint(equalTo: signInButton.topAnchor, constant: -Constants.padding),
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.widthProportion),
-            loginButton.heightAnchor.constraint(equalToConstant: Constants.buttonSize)
+            buttonsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Constants.padding),
+            buttonsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.widthProportion)
         ])
+        
+        buttons.forEach { button in
+            buttonsStackView.addArrangedSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: Constants.textFieldHeight).isActive = true
+        }
         
         signInButton.addTarget(self, action: #selector(onSignInButtonTap), for: .touchUpInside)
         loginButton.addTarget(self, action: #selector(onLoginButtonTap), for: .touchUpInside)
